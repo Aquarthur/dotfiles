d7ab5b0071eebc7bf3624aa8313513ed183e1142
Arthur Verkaik <art.v412@gmail.com>
2022-04-27 14:19:32 -0700

    Don't create symlink for zfunc folder within zsh/zfunc

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,18 +3,18 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 overwrite_with_check() {
-  if [[ -f $1 ]]; then
-    read -p "This will overwrite the existing $1. Are you sure? (y/n) " -n 1
+  if [[ -f $1 || -d $1 ]]; then
+    read -p "This will overwrite $1. Are you sure? (y/n) " -n 1
     echo ""
     if [[ $REPLY =~ ^[Yy]$ ]]; then
       echo "Overwriting $1 with $2..."
-      ln -sf $2 $1
+      ln -sTf $2 $1
     else
       echo "Moving on..."
     fi
   else
     echo "Could not find existing $1, creating from $2..."
-    ln -sf $2 $1
+    ln -sTf $2 $1
   fi
   echo ""
 }
